8a293ccb7a257b6bd2fe6e68fd377f9b8b9215ab
Marc Høgh <Marchogh@hotmail.com>
2020-08-06 00:51:15 +0200

    fixed dropdow on IOS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-let mobileBtn = document.querySelector('.mobile-menu'); 
-let menu = document.querySelector('.menu');
+const mobileBtn = document.querySelector(".mobile-menu");
+const menu = document.querySelector(".menu");
+const dropdownBtn = document.querySelector(".arrow-down");
+const dropdown = document.querySelector(".dropdown");
 
-    mobileBtn.addEventListener('click', () => {
-        menu.classList.toggle('open');
+mobileBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
 
-        if (menu.classList.contains('open')) {
-            menu.classList.remove('hidden');
-        } else {
-            menu.classList.add('hidden');
-        }
+  if (menu.classList.contains("open")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
 
-      });
-
+dropdownBtn.addEventListener("click", () => {
+  dropdown.classList.toggle("dropdown-open");
+  dropdown.style.opacity = "1";
+  if (dropdown.classList.contains("dropdown-open")) {
+    dropdown.classList.remove("dropdown-hidden");
+  } else {
+    dropdown.style.opacity = "0";
+  }
+});
